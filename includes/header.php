f860d296e8ca421542c33d66a02c4ac8e44c4ff5
scrooger98 <62050089+scrooger98@users.noreply.github.com>
2020-03-11 08:03:59 +0100

    Add files via upload

---
+++ includes/header.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<header>
+<link rel="stylesheet" href="style.css">
+<link href="https://fonts.googleapis.com/css?family=Open+Sans&display=swap" rel="stylesheet">
+<title>MEINE WEBSEITE</title>
+</header>
+<body>
+        
+<section>
+<div class="navbar">
+  <a href="#impressum">Impressum</a>    
+  <a href="#cars">Cars</a>
+  <div class="dropdown">
+    <button class="dropbtn">Dropdown 
+      <i class="fa fa-caret-down"></i>
+    </button>
+    <div class="dropdown-content">
+      <a href="#">Background Picture</a>
+      <a href="#">test</a>
+      <a href="#">Link 3</a>
+    </div>
+  </div>
+  <a href="#home">Home</a>
+</div>
+</section> 
